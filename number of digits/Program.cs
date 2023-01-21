2e991f3ce6bc205287b1fcb2b8af85c896dd257c
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-21 18:28:24 +0300

    number of digits

---
+++ number of digits/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string digits = number.ToString();
+
+System.Console.Write("Коливчество цифр в числе = ");
+System.Console.WriteLine(digits.Length-1 + 1);

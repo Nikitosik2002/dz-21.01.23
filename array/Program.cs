06371b0de1dc1f782dfc9de2a401684565317bb4
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-21 18:56:25 +0300

    print array

---
+++ array/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int[] array = new int[8];
+
+int number = 1;
+
+for (int i =0; i < array.Length; i++)
+{
+    System.Console.WriteLine("Введите число №" + number);
+    array[i] = Convert.ToInt16(Console.ReadLine());
+    number++;
+}
+System.Console.WriteLine("Вывод массива");
+
+for (int i =0; i < array.Length; i++)
+{
+    System.Console.WriteLine(array[i]);
+}

ac8fc7ab82fa2142a0be2a878786623d438f1a71
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-21 22:06:44 +0300

    multiplication 1-N

---
+++ multiplication/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число");
+var number = Convert.ToInt64(Console.ReadLine());
+
+int count = 1;
+int i = 1;
+
+while (i <= number)
+{
+    count = count * i;
+    i++;
+}
+System.Console.WriteLine(count);

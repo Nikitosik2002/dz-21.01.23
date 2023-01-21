f29c620fa896163a364e0bc5fbbb7eb800d4e6a9
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-21 20:25:48 +0300

    summ of array

---
+++ array 2/Program.cs
@@ -0,0 +1,47 @@
+﻿int[] array = new int[12];
+
+var random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0,9);
+}
+
+foreach (int j in array)
+{
+    System.Console.WriteLine(j);
+}
+
+int summPositive = 0;
+int summNegative = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+         summPositive += array[i];
+    }
+
+    if (array[i] < 0)
+    {
+         summNegative += array[i];
+    }
+    
+}
+
+if (summPositive == 0)
+{
+    System.Console.WriteLine("В массиве нет положительных чисел");
+}
+else
+{
+    System.Console.WriteLine("Сумма положительных чисел = " + summPositive);
+}
+
+if (summNegative == 0)
+{
+    System.Console.WriteLine("В массиве нет отрицательных чисел чисел");
+}
+else
+{
+    System.Console.WriteLine("Сумма отрицательных чисел = " + summNegative);
+}

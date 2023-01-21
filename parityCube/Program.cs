96db81db778941e691d5c606c0dbb177c1c4df29
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-21 22:22:30 +0300

    parity cube

---
+++ parityCube/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine("Введите число");
+var number = Convert.ToInt64(Console.ReadLine());
+
+int i = 1;
+int last;
+int cube;
+
+System.Console.WriteLine("Вывод куба всех чисел от 1 до " + number);
+while (i <= number)
+{
+    cube = i * i * i;
+    last = cube % 10;
+
+    if (last % 2 == 0)
+    {
+        System.Console.WriteLine(cube);
+    }
+    i++;
+}

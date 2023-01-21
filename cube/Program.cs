d1a377b2a9cc126d3e581d91226a0484ec677dee
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-21 15:28:13 +0300

    cube of number

---
+++ cube/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите число");
+var number = Convert.ToInt64(Console.ReadLine());
+
+int i = 1;
+System.Console.WriteLine("Вывод куба всех чисел от 1 до " + number);
+while (i <= number)
+{
+    System.Console.WriteLine(i*i*i);
+    i++;
+}

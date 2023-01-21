63ab013fd94c892fa05c4984d65852adfabd0641
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-21 22:49:08 +0300

    array of 8 elements, number definition

---
+++ arrayDZ/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] array = new int[8];
+
+var rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0, 2);
+}
+
+System.Console.WriteLine("Введите число (0 или 1) которое хотите найти в массиве");
+int seek = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Вывод массива на экран");
+
+int count = 0;
+
+foreach (int j in array)
+{
+    System.Console.WriteLine(j);
+
+    if(j == seek)
+    {
+        count++;
+    }
+}
+
+System.Console.WriteLine("В данном масиве цифра " + seek + " повторяется " + count + " раз");
+

afe4cb5187de315792e9fd90511f5f81beae091e
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-21 20:46:05 +0300

    A degree B

---
+++ degree/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number;
+
+System.Console.WriteLine("Введите степень");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i < degree)
+{
+    count = count * number;
+    i++;
+}
+
+System.Console.WriteLine(count);

b53066106a6df02eb56f18dfc9d55807d9ac3fb1
Nikitosik <nikita.kopeickin@yandex.ru>
2023-01-25 14:42:49 +0300

    lection 3

---
+++ sorting/Program.cs
@@ -0,0 +1,46 @@
+﻿var rnd = new Random();
+int [] array = new int[5];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(1, 5);
+}
+
+foreach (int j in array)
+{
+    System.Console.WriteLine(j);
+}
+
+System.Console.WriteLine();
+
+void sorting(int[] array)
+{
+for (int i = 0; i < array.Length-1; i++)
+{
+ int min = i;
+
+    for (int j = i+1; j < array.Length; j++)   
+    {
+        if (array[j] < array[min])
+        {
+            min = j;
+        }
+    }
+
+    int box = array[i];
+    array[i] = array[min];
+    array[min] = box;
+
+}
+}
+
+void printArray(int[] array) 
+{
+    foreach (int j in array)
+    {
+        System.Console.WriteLine(j);
+    }
+}
+
+sorting(array);
+printArray(array);
---
+++ sorting2/Program.cs
@@ -0,0 +1,36 @@
+﻿var rnd = new Random();
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(1,10);
+}
+void print (int[] array)
+{
+foreach (int i in array)
+{
+    System.Console.WriteLine(i);
+}
+}
+
+print(array);
+System.Console.WriteLine();
+
+for (int i = 0; i < array.Length - 1; i++)
+{
+    int max = i;
+    for (int j = i+1; j < array.Length; j++)
+    {
+        if (array[j] > array[max])
+        {
+            max = j;   
+        }
+    }
+
+    int box = array[i];
+    array[i] = array[max];
+    array[max] = box;
+}
+
+print(array);
